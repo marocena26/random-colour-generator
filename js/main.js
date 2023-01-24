d9e6939358caac6e75fe5834c5700a011d072da1
marocena26 <mariaelena.arocena@gmail.com>
2023-01-24 18:14:16 +0100

    add functionality - generate random colour with text

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 //CONSTANT
 const button = document.querySelector(".js-button");
 const background = document.querySelector(".js-background");
+const message = document.querySelector(".hidden");
 
 //VARIABLES
 //Variable to generate random colour
@@ -11,14 +12,18 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 //FUNCTIONS
 //Render a random colour
 const setBg = () => {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  document.body.style.backgroundColor = "#" + randomColor;
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 };
 
 //Handler function
 const handleColor = (ev) => {
   ev.preventDefault();
-  setBg();
+  const result = setBg();
+  background.style.background = result;
+  message.classList.remove("hidden");
+  message.innerHTML = `The selected colour has been the: ${result}`;
+  button.style.color = result;
+  button.style.borderColor = result;
 };
 
 //EVENTS
